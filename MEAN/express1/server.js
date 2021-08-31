de72f3ecd34361bfa24dc599feff2ad7e69c1aad
Kai Qi <55521333+kaidenqi@users.noreply.github.com>
2021-08-30 20:45:43 -0700

    8-30-2021

---
+++ MEAN/express1/server.js
@@ -0,0 +1,27 @@
+// let express = require('express')
+// let app = express()
+
+
+// // app.get("/", function(req, res){
+// //     res.send('Hello world from server.js')
+// // })
+
+// //tell express about static folder
+// app.use(express.static(__dirname+'/public'))
+
+// app.listen("3000", "127.0.0.1", function(){
+//     console.log("Server listening @3000")
+// })
+
+let express = require("express");
+let app = express();
+
+// app.get("/",(req,res)=>{
+//     res.send("Hello Express");
+// })
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen("3000", ()=>{
+    console.log("Server listening @3000");
+})

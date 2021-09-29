6e2a3006de6eafd631e6518190981e0ef803408a
Kai Qi <55521333+kaidenqi@users.noreply.github.com>
2021-09-29 14:02:32 -0700

    9-29

---
+++ ReactReduxDemo1/demo1/src/App2.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+export default function App2() {
+    const [age, setAge] = useState(20)
+    
+   
+   
+     return (
+        
+        <>
+            <h1>{age}</h1>
+          
+        </>
+    )
+}

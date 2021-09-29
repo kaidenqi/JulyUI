f2e717c295bb9e4e7d60bdd9c13ac5ae936ba55d
Kai Qi <55521333+kaidenqi@users.noreply.github.com>
2021-09-28 19:02:48 -0700

    9-28

---
+++ ReactReduxDemo/src/components/CakeContainer.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { buyCake } from '../redux'
+import { connect } from 'react-redux'
+
+//props 指向App.js里传过来的props，就是store
+function CakeContainer(props) {
+    return (
+        <>
+            <h1>Num of Cakes - {props.numOfCakes}</h1>
+            <button onClick={props.buyCake}>Buy Cake</button>
+        </>
+    )
+}
+
+//解释整个函数：As the first argument passed in to connect, mapStateToProps is used for
+// selecting the part of the data from the store that the connected component needs.
+
+// 解释函数的形参：The first argument to a mapStateToProps function is the entire Redux store 
+// state (the same value returned by a call to store.getState()).
+const mapStateToProps = state => {
+    return {
+        numOfCakes : state.cake.numOfCakes
+    }
+}
+//As the second argument passed in to connect, mapDispatchToProps is used for 
+// dispatching actions to the store.
+const mapDispatchToProps = dispatch => {
+    return{
+        buyCake: () => dispatch(buyCake())
+    }
+}
+
+//The connect() function connects a React component to a Redux store. 
+// 把react组件和存放数据的store关联起来
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer)

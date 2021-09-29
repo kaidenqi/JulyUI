38d4973f53df1ae3327af7bc69f1555c3a03102f
Kai Qi <55521333+kaidenqi@users.noreply.github.com>
2021-09-28 19:24:14 -0700

    9-28

--- ReactReduxDemo/src/redux/cakes/cakeReducer.js
+++ ReactReduxDemo/src/redux/cakes/cakeReducer.js
@@ -4,6 +4,9 @@ const initailState = {
     numOfCakes:10,
 }
 
+
+//Reducers are functions that take the current state and an action as arguments, 
+// and return a new state result. In other words, (state, action) => newState.
 const cakeReducer = (state = initailState, action) => {
     switch(action.type){
         case BUY_CAKE: return {
